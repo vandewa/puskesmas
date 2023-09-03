05f89bb4679aedbcc6cad969432a163259deccb5
vandewa <devandewananta@gmail.com>
2023-09-03 12:05:08 +0700

    ubah warna

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
       </div>
 
       <!-- Navbar -->
-      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <nav class="main-header navbar navbar-expand navbar-light">
           <!-- Left navbar links -->
           <ul class="navbar-nav">
               <li class="nav-item">
@@ -32,7 +32,7 @@
       <!-- /.navbar -->
 
       <!-- Main Sidebar Container -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <aside class="main-sidebar sidebar-light-success elevation-4">
           <!-- Brand Logo -->
           <a href="index3.html" class="brand-link">
               <img src="{{ asset('AdminLTE/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
