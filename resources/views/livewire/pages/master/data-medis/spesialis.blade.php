d1fb66a99a9010711728d1e5562343012e0c9c1f
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2023-10-03 07:56:26 +0700

    penambahan data spesialis

--- resources/views/livewire/pages/master/data-medis/spesialis.blade.php
+++ resources/views/livewire/pages/master/data-medis/spesialis.blade.php
@@ -1,4 +1,17 @@
 <div>
+    <x-slot name="header">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Master Data</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Master</a></li>
+                    <li class="breadcrumb-item active">Spesialis</li>
+                </ol>
+            </div>
+        </div>
+    </x-slot>
     <section class="content">
         <div class="container-fluid">
             <div class="row">
