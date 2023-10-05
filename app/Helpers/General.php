189769951dc289d887ba8d3796f1020c99a4d668
vandewa <devandewananta@gmail.com>
2023-10-05 18:56:46 +0700

    fix aray jacok

--- app/Helpers/General.php
+++ app/Helpers/General.php
@@ -21,34 +21,36 @@ use App\Models\His\TrxKelas;
 use App\Models\His\TrxInsurance;
 use App\Models\His\ComAccount;
 
-if(!function_exists('file_name')){
-    function file_name($path, $extension){
+if (!function_exists('file_name')) {
+    function file_name($path, $extension)
+    {
         $fileName = str_random(20);
-        while (Storage::disk('public')->exists($path.$fileName.$extension)){
+        while (Storage::disk('public')->exists($path . $fileName . $extension)) {
             $fileName = str_random(20);
         }
 
-        return $path.$fileName.'.'.$extension;
+        return $path . $fileName . '.' . $extension;
     }
 }
 
-if(!function_exists('get_code')){
-    function get_code($a){
-        $value = Cache::remember($a, config('app.cache_time'), function () use($a) {
-            return  ComCode::where('code_group', $a)->get()->toArray;
+if (!function_exists('get_code')) {
+    function get_code($a)
+    {
+        $value = Cache::remember($a, config('app.cache_time'), function () use ($a) {
+            return ComCode::where('code_group', $a)->get()->toArray();
         });
 
         return $value;
     }
 }
-if(!function_exists('tgl_indo')){
-    function tgl_indo($tanggal = ''){
-        if($tanggal == '')
-        {
+if (!function_exists('tgl_indo')) {
+    function tgl_indo($tanggal = '')
+    {
+        if ($tanggal == '') {
             return '-';
         }
-        $bulan = array (
-            1 =>   'Januari',
+        $bulan = array(
+            1 => 'Januari',
             'Februari',
             'Maret',
             'April',
@@ -67,19 +69,19 @@ if(!function_exists('tgl_indo')){
         // variabel pecahkan 1 = bulan
         // variabel pecahkan 2 = tahun
 
-        return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+        return $pecahkan[2] . ' ' . $bulan[(int) $pecahkan[1]] . ' ' . $pecahkan[0];
     }
 }
-if(!function_exists('file_url')){
-    function file_url($file = null, $default = 'http://placehold.it/400x400'){
+if (!function_exists('file_url')) {
+    function file_url($file = null, $default = 'http://placehold.it/400x400')
+    {
         if ($file !== null) {
-            if(is_array($file)){
+            if (is_array($file)) {
                 $images = [];
                 foreach ($file as $item) {
-                    if($item == 'http://placehold.it/400x400')
-                    {
+                    if ($item == 'http://placehold.it/400x400') {
                         array_push($images, 'http://placehold.it/400x400');
-                    }else{
+                    } else {
                         array_push($images, Storage::disk('public')->url($item));
                     }
                 }
@@ -94,12 +96,11 @@ if(!function_exists('file_url')){
     }
 }
 
-if(!function_exists('result')){
-    function result($result = '',$status = true, $messages = '', $kode = 200 ){
-        if(is_array($result))
-        {
-            if(count($result) === 0)
-            {
+if (!function_exists('result')) {
+    function result($result = '', $status = true, $messages = '', $kode = 200)
+    {
+        if (is_array($result)) {
+            if (count($result) === 0) {
                 $status = false;
             }
         }
@@ -116,204 +117,226 @@ if(!function_exists('result')){
     }
 }
 
-if(!function_exists('format_price')){
-    function format_price($value, $default = 'Rp '){
+if (!function_exists('format_price')) {
+    function format_price($value, $default = 'Rp ')
+    {
         $result = number_format($value, 0, ',', '.');
 
-        return $default.$result;
+        return $default . $result;
     }
 }
 
-if(!function_exists('list_code')){
-    function list_code($a){
-        $value = Cache::remember($a, config('app.cache_time'), function () use($a) {
-            return  ComCode::where('code_group', $a)->pluck('code_nm', 'com_cd');
+if (!function_exists('list_code')) {
+    function list_code($a)
+    {
+        $value = Cache::remember($a, config('app.cache_time'), function () use ($a) {
+            return ComCode::where('code_group', $a)->pluck('code_nm', 'com_cd');
         });
 
         return $value;
     }
 }
-if(!function_exists('list_bangsal')){
-    function list_bangsal(){
+if (!function_exists('list_bangsal')) {
+    function list_bangsal()
+    {
         $value = Cache::remember('list_bangsal', config('app.cache_time'), function () {
-            return  \App\Models\His\TrxBangsal::pluck('bangsal_nm', 'bangsal_cd');
+            return \App\Models\His\TrxBangsal::pluck('bangsal_nm', 'bangsal_cd');
         });
 
         return $value;
     }
 }
-if(!function_exists('list_kamar')){
-    function list_kamar(){
+if (!function_exists('list_kamar')) {
+    function list_kamar()
+    {
         $value = Cache::remember('list_kamar', config('app.cache_time'), function () {
-            return  \App\Models\His\TrxKamar::pluck('kamar_nm', 'kamar_cd');
+            return \App\Models\His\TrxKamar::pluck('kamar_nm', 'kamar_cd');
         });
 
         return $value;
     }
 }
-if(!function_exists('list_gudang')){
-    function list_gudang(){
+if (!function_exists('list_gudang')) {
+    function list_gudang()
+    {
         $value = Cache::remember('list_gudang', config('app.cache_time'), function () {
-            return  InvPosInventory::pluck('pos_nm', 'pos_cd');
+            return InvPosInventory::pluck('pos_nm', 'pos_cd');
         });
 
         return $value;
     }
 }
-if(!function_exists('list_kelas')){
-    function list_kelas(){
+if (!function_exists('list_kelas')) {
+    function list_kelas()
+    {
         $value = Cache::remember('list_kelas', config('app.cache_time'), function () {
-            return  TrxKelas::pluck('kelas_nm', 'kelas_cd');
+            return TrxKelas::pluck('kelas_nm', 'kelas_cd');
         });
 
         return $value;
     }
 }
-if(!function_exists('list_asuransi')){
-    function list_asuransi(){
+if (!function_exists('list_asuransi')) {
+    function list_asuransi()
+    {
         $value = Cache::remember('list_asuransi', config('app.cache_time'), function () {
-            return  TrxInsurance::pluck('insurance_nm', 'insurance_cd');
+            return TrxInsurance::pluck('insurance_nm', 'insurance_cd');
         });
 
         return $value;
     }
 }
-if(!function_exists('list_account')){
-    function list_account(){
+if (!function_exists('list_account')) {
+    function list_account()
+    {
         $value = Cache::remember('list_account', config('app.cache_time'), function () {
-            return  ComAccount::pluck('account_nm', 'account_cd');
+            return ComAccount::pluck('account_nm', 'account_cd');
         });
 
         return $value;
     }
 }
-if(!function_exists('list_referensi')){
-    function list_referensi(){
+if (!function_exists('list_referensi')) {
+    function list_referensi()
+    {
         return \App\Models\His\TrxReferensi::pluck('referensi_nm', 'referensi_cd');
     }
 }
-if(!function_exists('list_unit')){
-    function list_unit(){
-       return InvUnit::pluck('unit_nm', 'unit_cd');
+if (!function_exists('list_unit')) {
+    function list_unit()
+    {
+        return InvUnit::pluck('unit_nm', 'unit_cd');
     }
 }
-if(!function_exists('list_unit_medis')){
-    function list_unit_medis(){
-       return TrxUnitMedis::pluck('medunit_nm', 'medunit_cd');
-    }
-}
-
-
-if(!function_exists('list_jenis_inventori')){
-    function list_jenis_inventori(){
-       return InvItemType::pluck('type_nm', 'type_cd');
+if (!function_exists('list_unit_medis')) {
+    function list_unit_medis()
+    {
+        return TrxUnitMedis::pluck('medunit_nm', 'medunit_cd');
     }
 }
 
 
+if (!function_exists('list_jenis_inventori')) {
+    function list_jenis_inventori()
+    {
+        return InvItemType::pluck('type_nm', 'type_cd');
+    }
+}
 
-if(!function_exists('dow')){
-    function dow($a){
+
+
+if (!function_exists('dow')) {
+    function dow($a)
+    {
         $tgl = Carbon::parse($a);
         return $tgl->localeDayOfWeek;
     }
 }
-if(!function_exists('list_provinsi')){
-    function list_provinsi($a){
+if (!function_exists('list_provinsi')) {
+    function list_provinsi($a)
+    {
         $tgl = Carbon::parse($a);
         return $tgl->localeDayOfWeek;
     }
 }
-if(!function_exists('gen_pasien_cd')){
-    function gen_pasien_cd(){
-        $no = Date('y').str_pad(1, 8, '0', STR_PAD_LEFT );
-        $terakhir = \App\Models\His\TrxPasien::orderBy('created_at','desc')->first();
-        if($terakhir)
-        {
-            $no = Date('y').str_pad((int)substr($terakhir->pasien_cd,-8) + 1,8,0,STR_PAD_LEFT);
+if (!function_exists('gen_pasien_cd')) {
+    function gen_pasien_cd()
+    {
+        $no = Date('y') . str_pad(1, 8, '0', STR_PAD_LEFT);
+        $terakhir = \App\Models\His\TrxPasien::orderBy('created_at', 'desc')->first();
+        if ($terakhir) {
+            $no = Date('y') . str_pad((int) substr($terakhir->pasien_cd, -8) + 1, 8, 0, STR_PAD_LEFT);
         }
         return $no;
     }
 }
 
-if(!function_exists('gen_no_rm')){
-    function gen_no_rm(){
-        $no = str_pad(1, 8, '0', STR_PAD_LEFT );
-        $terakhir = \App\Models\His\TrxPasien::orderBy('created_at','desc')->first();
-        if($terakhir)
-        {
-            $no = str_pad((int)substr($terakhir->no_rm,-8) + 1,8,0,STR_PAD_LEFT);
+if (!function_exists('gen_no_rm')) {
+    function gen_no_rm()
+    {
+        $no = str_pad(1, 8, '0', STR_PAD_LEFT);
+        $terakhir = \App\Models\His\TrxPasien::orderBy('created_at', 'desc')->first();
+        if ($terakhir) {
+            $no = str_pad((int) substr($terakhir->no_rm, -8) + 1, 8, 0, STR_PAD_LEFT);
         }
         return $no;
     }
 }
-if(!function_exists('gen_no_group')){
-    function gen_no_group(){
-        $no = date('Y').str_pad(1, 8, '0', STR_PAD_LEFT );
-        $terakhir = \App\Models\His\TrxMedicalUnit::whereYear('created_at', date('Y'))->orderBy('created_at','desc')->first();
-        if($terakhir)
-        {
-            $no = date('Y').str_pad((int)substr($terakhir->group_no,-8) + 1,8,0,STR_PAD_LEFT);
+if (!function_exists('gen_no_group')) {
+    function gen_no_group()
+    {
+        $no = date('Y') . str_pad(1, 8, '0', STR_PAD_LEFT);
+        $terakhir = \App\Models\His\TrxMedicalUnit::whereYear('created_at', date('Y'))->orderBy('created_at', 'desc')->first();
+        if ($terakhir) {
+            $no = date('Y') . str_pad((int) substr($terakhir->group_no, -8) + 1, 8, 0, STR_PAD_LEFT);
         }
         return $no;
     }
 }
-if(!function_exists('gen_medical_cd')){
-    function gen_medical_cd(){
-        $no = Date('y').str_pad(1, 8, '0', STR_PAD_LEFT );
-        $terakhir = \App\Models\His\TrxMedical::whereYear('created_at', date('Y'))->orderBy('created_at','desc')->first();
-        if($terakhir)
-        {
-            $no = Date('y').str_pad((int)substr($terakhir->medical_cd,-8) + 1,8,0,STR_PAD_LEFT);
+if (!function_exists('gen_medical_cd')) {
+    function gen_medical_cd()
+    {
+        $no = Date('y') . str_pad(1, 8, '0', STR_PAD_LEFT);
+        $terakhir = \App\Models\His\TrxMedical::whereYear('created_at', date('Y'))->orderBy('created_at', 'desc')->first();
+        if ($terakhir) {
+            $no = Date('y') . str_pad((int) substr($terakhir->medical_cd, -8) + 1, 8, 0, STR_PAD_LEFT);
         }
         return $no;
     }
 }
-if(!function_exists('get_dokter')){
-    function get_dokter(){
+if (!function_exists('get_dokter')) {
+    function get_dokter()
+    {
         $data = \App\Models\His\TrxDokter::pluck('dr_nm', 'dr_cd');
         return $data;
     }
 }
-if(!function_exists('get_paramedis')){
-    function get_paramedis(){
-        $data = \App\Models\His\TrxParamedis::pluck('paramedis_nm','paramedis_cd');
+if (!function_exists('get_paramedis')) {
+    function get_paramedis()
+    {
+        $data = \App\Models\His\TrxParamedis::pluck('paramedis_nm', 'paramedis_cd');
         return $data;
     }
 }
-if(!function_exists('get_all_paramedis')){
-    function get_all_paramedis(){
-        $data = \App\Models\His\TrxParamedis::pluck('paramedis_nm','paramedis_cd');
+if (!function_exists('get_all_paramedis')) {
+    function get_all_paramedis()
+    {
+        $data = \App\Models\His\TrxParamedis::pluck('paramedis_nm', 'paramedis_cd');
         $paramedis = get_dokter()->merge($data);
         return $paramedis;
     }
 }
-if(!function_exists('get_poli')){
-    function get_poli(){
+if (!function_exists('get_poli')) {
+    function get_poli()
+    {
         $data = TrxUnitMedis::whereMedicalunitTp('MEDICALUNIT_TP_1')->pluck('medunit_nm', 'medunit_cd');
         return $data;
     }
 }
-if(!function_exists('get_lab')){
-    function get_lab(){
-        $data = TrxUnitMedis::where('medicalunit_tp','MEDICALUNIT_TP_2')->pluck('medunit_nm', 'medunit_cd');
+if (!function_exists('get_lab')) {
+    function get_lab()
+    {
+        $data = TrxUnitMedis::where('medicalunit_tp', 'MEDICALUNIT_TP_2')->pluck('medunit_nm', 'medunit_cd');
         return $data;
     }
 }
-if(!function_exists('get_rad')){
-    function get_rad(){
-        $data = TrxUnitMedis::where('medicalunit_tp','MEDICALUNIT_TP_3')->pluck('medunit_nm', 'medunit_cd');
+if (!function_exists('get_rad')) {
+    function get_rad()
+    {
+        $data = TrxUnitMedis::where('medicalunit_tp', 'MEDICALUNIT_TP_3')->pluck('medunit_nm', 'medunit_cd');
         return $data;
     }
 }
-if(!function_exists('get_info_pasien')){
-    function get_info_pasien($a){
+if (!function_exists('get_info_pasien')) {
+    function get_info_pasien($a)
+    {
         $data = TrxMedical::with('pasien', 'jenisKunjungan', 'jenisPasien', 'asuransi', 'status')->find($a);
         return $data;
     }
 }
-if(!function_exists('hitung_umur')){
-    function hitung_umur($tanggal_lahir){
+if (!function_exists('hitung_umur')) {
+    function hitung_umur($tanggal_lahir)
+    {
         $birthDt = new DateTime($tanggal_lahir);
         //tanggal hari ini
         $today = new DateTime('today');
@@ -323,31 +346,28 @@ if(!function_exists('hitung_umur')){
         $m = $today->diff($birthDt)->m;
         //hari
         $d = $today->diff($birthDt)->d;
-        return  $y . " tahun " . $m . " bulan " . $d . " hari";
+        return $y . " tahun " . $m . " bulan " . $d . " hari";
     }
 }
 
-if(!function_exists('code_item_master')){
-    function code_item_master(){
-        $no = str_pad(1, 8, '0', STR_PAD_LEFT );
-        $terakhir = \App\Models\His\InvItemMaster::orderBy('created_at','desc')->first();
-        if($terakhir)
-        {
-            $no = str_pad((int)substr($terakhir->item_cd,-8) + 1,8,0,STR_PAD_LEFT);
+if (!function_exists('code_item_master')) {
+    function code_item_master()
+    {
+        $no = str_pad(1, 8, '0', STR_PAD_LEFT);
+        $terakhir = \App\Models\His\InvItemMaster::orderBy('created_at', 'desc')->first();
+        if ($terakhir) {
+            $no = str_pad((int) substr($terakhir->item_cd, -8) + 1, 8, 0, STR_PAD_LEFT);
         }
         return $no;
     }
 }
-if(!function_exists('instansi')){
-    function instansi(){
+if (!function_exists('instansi')) {
+    function instansi()
+    {
         $value = Cache::remember('rs', config('app.cache_time'), function () {
-            return  TrxRs::with(['provinsi','kabupaten', 'kecamatan', 'kelurahan'])->firstOrCreate(['rs_cd' => config('app.rs_code')]);
+            return TrxRs::with(['provinsi', 'kabupaten', 'kecamatan', 'kelurahan'])->firstOrCreate(['rs_cd' => config('app.rs_code')]);
         });
 
         return $value;
     }
 }
-
-
-
-
