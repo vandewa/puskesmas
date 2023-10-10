cdb61f79a5b80d4a9b4e9fd5457b1334b0d50e4d
vandewa <devandewananta@gmail.com>
2023-10-10 20:34:01 +0700

    tambah welcome

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts/app')
 
 @section('content')
-    <section class="content">
+    <div id="idnsticky_bar">
+        <div id="sticybaricon">
+            <a href="#" id="cs-logo">
+                <span></span> </a>
+        </div>
+        <div id="sticky_bar_text">
+            <p class="customdesco">
+                <span style="font-family: 'Baloo Tammudu 2'; color:white;">Selamat Datang di </span>
+            </p>
+        </div>
+        <div id="sticky_bar_btn">
+            <a href="#"><button class="btn-hover color-2">E-RM Puskesmas Sapuran</button></a>
+        </div>
+    </div>
+
+
+    <section class="content mt-5">
         <div class="container-fluid">
+
             <!-- Small boxes (Stat box) -->
             <div class="box-body card-success card-outline" style="background-color:white; padding-bottom:20px;">
                 <div class="box-header">
@@ -75,6 +92,80 @@
 
         .jarak {
             padding: 15px;
+        }
+    </style>
+    <style>
+        #idnsticky_bar {
+            padding: 8px 5px;
+            background-image: linear-gradient(to right, #287a83, #15ab04, #15ab04, #287a83);
+            position: -webkit-sticky;
+            top: 0;
+            z-index: 999;
+            text-align: center;
+            box-shadow: 0px 1px 11px #888888;
+            color: #000000;
+            font-family: 'Viga', sans-serif;
+            font-size: 1.1em;
+            display: block;
+        }
+
+        #sticky_bar_text,
+        #sticybaricon,
+        #sticky_bar_btn {
+            display: inline-block;
+        }
+
+        #cs-logo {
+            height: 27px;
+            padding-top: 7px;
+            background: url(https://app.crowdsignal.com/images/logo-white.svg) no-repeat center center;
+            background-size: 30px 30px;
+            width: 40px;
+            color: transparent;
+            display: list-item;
+        }
+
+        .btn-hover {
+            font-size: 16px;
+            font-weight: 600;
+            color: #fff;
+            cursor: pointer;
+            padding: 10px 15px;
+            text-align: center;
+            border: none;
+            background-size: 300% 100%;
+            border-radius: 50px;
+            moz-transition: all .4s ease-in-out;
+            -o-transition: all .4s ease-in-out;
+            -webkit-transition: all .4s ease-in-out;
+            transition: all .4s ease-in-out;
+        }
+
+        .btn-hover:hover {
+            background-position: 100% 0;
+            moz-transition: all .4s ease-in-out;
+            -o-transition: all .4s ease-in-out;
+            -webkit-transition: all .4s ease-in-out;
+            transition: all .4s ease-in-out;
+        }
+
+        p.customdesco {
+            margin: 0;
+            font-weight: 300;
+        }
+
+        .btn-hover:focus {
+            outline: none;
+        }
+
+        .btn-hover.color-1 {
+            background-image: linear-gradient(to right, #eb3941, #f15e64, #e14e53, #e2373f);
+            box-shadow: 0 5px 15px rgba(242, 97, 103, .4);
+        }
+
+        .btn-hover.color-2 {
+            background-color: white;
+            color: #000000;
         }
     </style>
 @endpush
