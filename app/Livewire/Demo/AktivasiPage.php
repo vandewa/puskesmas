446192d620f919e0343f800860d98bb56419a7cf
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2023-11-24 23:21:32 +0700

    penambahan ukuran max upload

--- app/Livewire/Demo/AktivasiPage.php
+++ app/Livewire/Demo/AktivasiPage.php
@@ -24,7 +24,7 @@ class AktivasiPage extends Component
     public function save()
     {
         $this->validate([
-            'bukti' => 'image|max:2048'
+            'bukti' => 'image|max:20480'
         ]);
         $a = $this->bukti->store('public/bukti');
 
