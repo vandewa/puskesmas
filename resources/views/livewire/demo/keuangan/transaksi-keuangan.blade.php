9867efec986fedb7eb1321e9da1ea61582679bc4
vandewa <devandewananta@gmail.com>
2023-12-30 13:56:16 +0700

    hilang submit

--- resources/views/livewire/demo/keuangan/transaksi-keuangan.blade.php
+++ resources/views/livewire/demo/keuangan/transaksi-keuangan.blade.php
@@ -78,10 +78,10 @@
 
                                                                     </div>
                                                                 </div>
-                                                                <div class="card-footer">
+                                                                {{-- <div class="card-footer">
                                                                     <button type="submit"
                                                                         class="btn btn-info">Simpan</button>
-                                                                </div>
+                                                                </div> --}}
                                                             </form>
                                                         </div>
                                                         <br>
