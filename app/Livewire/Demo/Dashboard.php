e81492ed56762241a77368186d0cce3f853b5981
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2024-01-02 16:57:32 +0700

    penambahan redirect

--- app/Livewire/Demo/Dashboard.php
+++ app/Livewire/Demo/Dashboard.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public function mount() {
+
+        if(auth()->user()->hasRole('user')){
+            $this->redirect(AktivasiPage::class);
+        }
+    }
     public function render()
     {
         return view('livewire.demo.dashboard');
