f8d19d3be4fb159b7437bb620a6abfef5f562dd8
vandewa <devandewananta@gmail.com>
2024-01-07 22:37:58 +0700

    perbaikan menu user

--- app/Livewire/Pages/Admin/UserIndex.php
+++ app/Livewire/Pages/Admin/UserIndex.php
@@ -10,13 +10,49 @@ class UserIndex extends Component
 {
 
     use WithPagination;
-    public $cari;
+    public $cari, $idHapus;
+
+    public function delete($id)
+    {
+        $this->idHapus = $id;
+        $this->js(<<<'JS'
+        Swal.fire({
+            title: 'Apakah Anda yakin?',
+                text: "Apakah kamu ingin menghapus data ini? proses ini tidak dapat dikembalikan.",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Hapus!',
+                cancelButtonText: 'Batal'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                $wire.hapus()
+            }
+          })
+        JS);
+    }
+
+
+    public function hapus()
+    {
+        User::destroy($this->idHapus);
+        $this->js(<<<'JS'
+        Swal.fire({
+            title: 'Good job!',
+            text: 'You clicked the button!',
+            icon: 'success',
+          })
+        JS);
+    }
 
     public function render()
     {
-        $data = User::cari($this->cari)->whereHas('roles', function ($a) {
-            $a->where('name', 'user');
-        })->paginate(10);
+        $data = User::cari($this->cari)
+            // ->whereHas('roles', function ($a) {
+            //     $a->where('name', 'user');
+            // })
+            ->paginate(10);
 
         return view('livewire.pages.admin.user-index', [
             'posts' => $data
--- resources/views/livewire/pages/admin/user-index.blade.php
+++ resources/views/livewire/pages/admin/user-index.blade.php
@@ -1,71 +1,98 @@
 <div>
     <x-slot name="header">
-        <div class="mb-2 row">
+        <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Siswa</h1>
+                <h1 class="m-0">User</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Admin</a></li>
-                    <li class="breadcrumb-item active">Siswa
+                    <li class="breadcrumb-item active">User</li>
+                </ol>
             </div>
         </div>
     </x-slot>
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
-                    <div class="card card-info">
-                        <div class="card-header">
-                            <h3 class="card-title"> Siswa</h3>
-                            <div class="card-tools">
-                                <div class="input-group input-group-sm" style="width: 150px;">
-                                    <input type="text" name="table_search" class="float-right form-control "
-                                        placeholder="Search" wire:model.live='cari'>
+                <div class="col-md-12">
+                    <div class="card-body">
+                        <div class="tab-pane fade active show">
+                            <div class="tab-pane active show fade" id="custom-tabs-one-rm" role="tabpanel"
+                                aria-labelledby="custom-tabs-one-rm-tab">
+                                <div class="row">
+                                    <div class="col-md-12">
 
-                                    <div class="input-group-append">
-                                        <button type="submit" class="btn btn-default">
-                                            <i class="fas fa-search"></i>
-                                        </button>
+                                        <div class="card card-success card-outline   card-tabs">
+                                            <div class="tab-content" id="custom-tabs-six-tabContent">
+                                                <div class="tab-pane fade show active" id="custom-tabs-six-riwayat-rm"
+                                                    role="tabpanel" aria-labelledby="custom-tabs-six-riwayat-rm-tab">
+                                                    <div class="card-body">
+                                                        <div class="card-header">
+                                                            {{-- <div class="card-title">
+                                                                Role
+                                                            </div> --}}
+                                                            <div class="text-left mb-3">
+                                                                <a href="{{ route('master.user') }}"
+                                                                    class="btn btn-info"><i
+                                                                        class="fas fa-plus-square mr-2"></i>Tambah
+                                                                    Data</a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            <div class="row">
+                                                                <div class="col-md-2">
+                                                                    <input type="text" class="form-control"
+                                                                        placeholder="cari" wire:model.live='cari'>
+                                                                </div>
+                                                            </div>
+
+                                                            <div class="table-responsive">
+                                                                <table class="table">
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <th>Nama</th>
+                                                                            <th>Email</th>
+                                                                            <th>Role</th>
+                                                                            <th>Aksi</th>
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                        @foreach ($posts as $item)
+                                                                            <tr>
+                                                                                <td>{{ $item->name ?? '' }}</td>
+                                                                                <td>{{ $item->email ?? '' }}</td>
+                                                                                <td>{{ $item->roles()->first()->name ?? '-' }}
+                                                                                </td>
+                                                                                <td>
+                                                                                    <a href="{{ route('master.user', $item->id) }}"
+                                                                                        class="btn btn-sm btn-primary mr-2">Detail
+                                                                                    </a>
+
+                                                                                    <button type="button"
+                                                                                        class="btn btn-sm btn-danger"
+                                                                                        wire:click="delete('{{ $item->id }}')">Hapus
+                                                                                    </button>
+                                                                                </td>
+                                                                            </tr>
+                                                                        @endforeach
+                                                                    </tbody>
+                                                                </table>
+                                                            </div>
+                                                            {{ $posts->links() }}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- /.card -->
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- /.card-header -->
-                        <div class="card-body ">
-                            <div class="row">
-                                <div class="p-0 table-responsive">
-                                    <table class="table table-hover text-nowrap">
-                                        <thead>
-                                            <tr>
-                                                <th>Nama</th>
-                                                <th>Email</th>
-                                                <th>Role</th>
-                                                <th>Aksi</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($posts as $item)
-                                                <tr>
-                                                    <td>{{ $item->name }}</td>
-                                                    <td>{{ $item->email }}</td>
-                                                    <td>{{ $item->roles()->first()->name ?? '-' }}</td>
-                                                    <td>
-                                                        <a href="#" class="btn btn-sm btn-primary">Detail</button>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                    {{ $posts->links() }}
-                                </div>
-                            </div>
-
-                        </div>
                     </div>
-                    <!-- /.card-body -->
+                    <!-- /.card -->
                 </div>
-                <!-- /.card -->
             </div>
         </div>
     </section>
