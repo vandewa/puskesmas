c0442cf0397ed37ae97a74d74bf7ab97de786cd8
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2024-01-08 00:03:57 +0700

    pembtasan upload to 5mb

--- app/Livewire/Component/Document/FormDocument.php
+++ app/Livewire/Component/Document/FormDocument.php
@@ -57,7 +57,7 @@ class FormDocument extends Component
 
         $this->validate([
             'form.document_tp' => 'required',
-            'path' => 'required|image',
+            'path' => 'required|image|max:5120',
         ]);
        $a = $this->path->store('path');
         $this->form['path'] = $a;
