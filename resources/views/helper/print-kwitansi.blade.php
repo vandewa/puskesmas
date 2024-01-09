f8ee57abfb66948b3dbcb3efd43b687e6e008969
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2024-01-09 22:44:47 +0700

    perbaikan css

--- resources/views/helper/print-kwitansi.blade.php
+++ resources/views/helper/print-kwitansi.blade.php
@@ -10,9 +10,9 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('AdminLte') }}/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('AdminLTE') }}/plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('AdminLte') }}/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ asset('AdminLTE') }}/dist/css/adminlte.min.css">
 </head>
 
 <body>
